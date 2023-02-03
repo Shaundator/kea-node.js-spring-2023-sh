e9a1f43232a2300338d3f3bdca15f050c9ffe990
Shaundator <rash0081@stud.kea.dk>
2023-02-03 13:29:23 +0100

    exercise 2

---
+++ 01._Intro_Exercises/1.Basics.js
@@ -0,0 +1,37 @@
+// --------------------------------------
+// Variables, strings, numbers, floats
+// --------------------------------------
+// Exercise 1 - Console and constiables
+
+const firstName = "Anders";
+const lastName = "Latif";
+// EXERCISE
+// show in the console
+// My first name is Anders and my last name is Latif
+console.log('\nExercise 1:')
+const introdocution1  = `My first name is ${firstName} and my last name is ${lastName}\n`
+const introdocution2  = 'My first name is '+ firstName + ' and my last name is ' + lastName + '\n'
+
+console.log('introduction 1: ' + introdocution1)
+console.log('introduction 2: ' + introdocution2)
+
+// --------------------------------------
+// Exercise 2 - Numbers and Strings
+
+const year = "2022";
+const number = 1;
+
+// Add the year plus the number
+// The result should be 2020
+// You cannot touch line 1 or 2
+
+
+
+console.log('Exercise 2:')
+console.log(parseInt(year)+number)
+console.log(Number(year)+number)
+console.log( +year + number)
+// Question: Which one is the right one to use?
+
+
+// --------------------------------------
---
+++ 01._Intro_Exercises/2.Strings.js
@@ -0,0 +1,74 @@
+// --------------------------------------
+// Exercise 3 - Add numbers from string to float
+
+const numberOne = "1.10";
+const numberTwo = "2.30";
+
+// add those two numbers and show the result
+// you cannot touch line 1 neither line 2
+
+const exercise3 = parseFloat(numberOne) + parseFloat(numberTwo)
+console.log(exercise3)
+// --------------------------------------
+
+
+// --------------------------------------
+// Exercise 4 - Add the numbers and the total with 2 decimals
+
+const anotherNumberOne = "1.10";
+const anotherNumberTwo = "2.30";
+
+const another1 = parseFloat(numberOne);
+const another2 = parseFloat(numberTwo);
+const exercise4 = another1 + another2;
+
+console.log(exercise4.toFixed(2));
+
+
+// --------------------------------------
+// Exercise 5 - Decimals and average
+
+const one = 10;
+const two = 45;
+const three = 98;
+
+// Show in the console the avg. with 5 decimals
+
+const exercise5 = (one + two + three) / 3
+
+console.log(parseFloat(exercise5).toFixed(5))
+
+
+
+// --------------------------------------
+// Exercise 6 - Get the character by index
+
+const letters = "abc";
+// Get me the character "c"
+
+const exercise6 = letters.charAt(2)
+//letters[2] also works
+
+console.log(exercise6)
+
+
+
+
+// --------------------------------------
+// Exercise 7 - Replace
+
+const fact = "You are learning javascript!";
+
+// capitalize the J in Javascript
+
+const location = fact.indexOf("javascript")
+const temporaryExercise7 = fact.split("")
+temp[location] = "J"
+const exercise7 = temp.join("")
+
+console.log(exercise7)
+
+
+// --------------------------------------
+
+
